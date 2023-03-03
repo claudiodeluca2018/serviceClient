98f33eb19cdb1f242dfe5e4732429355b2780081
claudiodeluca2018 <claudiodeluca2018@gmail.com>
2023-03-03 10:11:38 -0300

    agregue calculation-proccess.cs

--- calculation-proccess.cs
+++ calculation-proccess.cs
@@ -1,2 +1,7 @@
 //servicio de proceso el cual tiene la responsabilidad de procesar informacion relacionada con los clientes
 //y entregar respuestas significativas para que otros servicios de clientes las formateen y las entreguen 
+
+function TaxesData CalulateTaxesData(DataClient dataClient)
+{
+    return new TaxesData();
+}
