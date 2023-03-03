34220e86c9e000f4559124ef3791f85fff0e2803
claudiodeluca2018 <claudiodeluca2018@gmail.com>
2023-03-03 10:12:26 -0300

    agregue service-client.cs

--- service-client.cs
+++ service-client.cs
@@ -1,1 +1,6 @@
 //servicio de clientes el cual contiene funciones relacionadas con los clientes, abm e informacion util
+
+function DataClient getClientData(IdentifierClient idClient)
+{
+   return new DataClient();
+}
